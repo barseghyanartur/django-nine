143194337ea5ae694ed5a39b75dce14106504289
Artur Barseghyan <artur.barseghyan@gmail.com>
2022-12-05 21:36:25 +0100

    Update testing reqs

--- scripts/compile_requirements.sh
+++ scripts/compile_requirements.sh
@@ -1,65 +1,71 @@
 #!/usr/bin/env bash
 cd examples/requirements/
 
-echo "pip-compile base.in"
-pip-compile base.in "$@"
+#echo "\n\npip-compile base.in"
+#pip-compile base.in "$@"
 
-echo "pip-compile common.in"
-pip-compile common.in "$@"
+#echo "\n\npip-compile common.in"
+#pip-compile common.in "$@"
 
-echo "pip-compile debug.in"
+echo "\n\npip-compile debug.in"
 pip-compile debug.in "$@"
 
-echo "pip-compile django_1_4.in"
+echo "\n\npip-compile django_1_4.in"
 pip-compile django_1_4.in "$@"
 
-echo "pip-compile django_1_5.in"
+echo "\n\npip-compile django_1_5.in"
 pip-compile django_1_5.in "$@"
 
-echo "pip-compile django_1_6.in"
+echo "\n\npip-compile django_1_6.in"
 pip-compile django_1_6.in "$@"
 
-echo "pip-compile django_1_7.in"
+echo "\n\npip-compile django_1_7.in"
 pip-compile django_1_7.in "$@"
 
-echo "pip-compile django_1_8.in"
+echo "\n\npip-compile django_1_8.in"
 pip-compile django_1_8.in "$@"
 
-echo "pip-compile django_1_9.in"
+echo "\n\npip-compile django_1_9.in"
 pip-compile django_1_9.in "$@"
 
-echo "pip-compile django_1_10.in"
+echo "\n\npip-compile django_1_10.in"
 pip-compile django_1_10.in "$@"
 
-echo "pip-compile django_1_11.in"
+echo "\n\npip-compile django_1_11.in"
 pip-compile django_1_11.in "$@"
 
-echo "pip-compile django_2_0.in"
+echo "\n\npip-compile django_2_0.in"
 pip-compile django_2_0.in "$@"
 
-echo "pip-compile django_2_1.in"
+echo "\n\npip-compile django_2_1.in"
 pip-compile django_2_1.in "$@"
 
-echo "pip-compile django_2_2.in"
+echo "\n\npip-compile django_2_2.in"
 pip-compile django_2_2.in "$@"
 
-echo "pip-compile django_3_0.in"
+echo "\n\npip-compile django_3_0.in"
 pip-compile django_3_0.in "$@"
 
-echo "pip-compile django_3_1.in"
+echo "\n\npip-compile django_3_1.in"
 pip-compile django_3_1.in "$@"
 
-echo "pip-compile django_3_2.in"
+echo "\n\npip-compile django_3_2.in"
 pip-compile django_3_2.in "$@"
 
-echo "pip-compile django_master.in"
+echo "\n\npip-compile django_4_0.in"
+pip-compile django_4_0.in "$@"
+
+echo "\n\npip-compile django_4_1.in"
+pip-compile django_4_1.in "$@"
+
+echo "\n\npip-compile django_master.in"
 pip-compile django_master.in "$@"
 
-echo "pip-compile docs.in"
+echo "\n\npip-compile docs.in"
 pip-compile docs.in "$@"
 
-echo "pip-compile style_checkers.in"
+echo "\n\npip-compile style_checkers.in"
 pip-compile style_checkers.in "$@"
 
-echo "pip-compile testing.in"
+echo "\n\npip-compile testing.in"
 pip-compile testing.in "$@"
